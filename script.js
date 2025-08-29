b0217b9cacae539598f11d0433d19f594e3674db
HeadyBroz <nharnish2@gmail.com>
2025-08-29 19:18:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,20 +17,20 @@ document.addEventListener("DOMContentLoaded", () => {
                     - $25 8th / $7.14 a G - $90 half<br>
                     <span style="color: #00e676;">Strains: SOLD OUT </span><br>
                     - $20 8th / $5 a G<br>
-                    <span style="color: #00e676;">Strains: Blue Ice LIMITED STOCK , Bolo Runtz, Titty Sprinkles, Sherbcake, </span><br>
+                    <span style="color: #00e676;">Strains: Bolo Runtz, Titty Sprinkles, Sherbcake, </span><br>
                     <span style="color: #00e676;">Lulu</span></p>
-                    <p><strong style="color: #6200ea;">High Tide Hash Rosin Disposable (0.5g)</strong><br>
-                    - $35 for one - $65 for two<br>
-                    <span style="color: #00e676;">Strains: Zellos, Mad Honey, Sherbet, Biscotti Chunks, Purple Ice Water, Black Cherry Guava</span></p>
+                    <p><strong style="color: #6200ea;">Eclipse Live Resin Disposable (1g)</strong><br>
+                    - $35 for one - $60 for two<br>
+                    <span style="color: #00e676;">Strains: TBD</span></p>
                     <p><strong style="color: #6200ea;">Seréno Hash Rosin Disposable (0.5g)</strong><br>
                     - $30 for one - $55 for two<br>
-                    <span style="color: #00e676;">Strains: Banana Ringz</span></p>
+                    <span style="color: #00e676;">Strains: SOLD OUT </span></p>
                     <p><strong style="color: #6200ea;">HeadyBroz Pre-Rolls</strong><br>
                     - King Tree (1g king size joint): $15 for one, $20 for two<br>
                     - Mini Trees (2 - 0.5g joints): $15 for one, $25 for two<br>
                     <span style="color: #00e676;">Strain: Candy Chrome - Infused and coated with Premium Distillate and Kief</span></p>
                     <p><strong style="color: #6200ea;">High Tides Hash Rosin</strong><br>
-                    - $90 2gram bucket</p>
+                    - $90 2gram bucket SOLD OUT</p>
                                 <p><strong style="color: #6200ea;">Maine Sourced Hash Rosin</strong><br>
                     - $40 per gram Animal Tsunami</p> 
                 </div>
