1d474018eb6ed4914a677e90bc8b6d3689cd3609
HeadyBroz <nharnish2@gmail.com>
2026-02-09 08:11:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ document.addEventListener("DOMContentLoaded", () => {
                     - $20 8th / $5 a G<br>
                     <span style="color: #00e676;">Strains: Bolo Runtz, Lemon Cherry Runtz, Bubblegum Yumz </span><br>
                     <span style="color: #00e676;"></span></p>
-                    <p><strong style="color: #6200ea;">Eclipse Live Resin Disposable (1g)</strong><br>
+                    <p><strong style="color: #6200ea;">Maine Sourced Live Resin Disposable (1g)</strong><br>
                     - $35 for one - $60 for two<br>
-                    <span style="color: #00e676;">Strains: Lemon Ice, Pink Zkittlez, Sherberry, Sherb Cream Pie, Sugar Factory, Super Boof</span></p>
+                    <span style="color: #00e676;">Strains: Lemon Berry, GMO</span></p>
                     <p><strong style="color: #6200ea;">Seréno Hash Rosin Disposable (0.5g)</strong><br>
                     - $30 for one - $55 for two<br>
-                    <span style="color: #00e676;">Strains: Petro-Chem </span></p>
+                    <span style="color: #00e676;">Strains: SOLD OUT </span></p>
                     <p><strong style="color: #6200ea;">HeadyBroz Pre-Rolls</strong><br>
                     - King Tree (1g king size joint): $15 for one, $20 for two<br>
                     - Mini Trees (2 - 0.5g joints): $15 for one, $25 for two<br>
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     <p><strong style="color: #6200ea;">High Tides Hash Rosin</strong><br>
                     - $90 2gram bucket SOLD OUT</p>
                                 <p><strong style="color: #6200ea;">Maine Sourced Hash Rosin</strong><br>
-                    - $40 per gram Animal Tsunami</p> 
+                    - $40 per gram Animal Tsunami, $80 2g Bucket Grapes N' Cream</p> 
                 </div>
             `,
             confirmButtonText: "Close",
