e77b6e9d9434df06b58ec33192b0a03daf4461ae
HeadyBroz <nharnish2@gmail.com>
2026-02-18 22:40:58 -0500

    Update script.js
    Pictures all added

--- script.js
+++ script.js
@@ -19,10 +19,12 @@ menuButton.addEventListener("click", () => {
                 - $20 8th / $5 a G<br>
                 <span style="color: #00e676;">Strains: Brain Freeze </span><br>
                 <span style="color: #00e676;"></span></p>
+                <img src="Images/brainfreeze.jpeg" alt="brainfreeze" style="max-width: 100%; max-height: 300px; height: auto; width: auto; display: block; margin: 10px auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.2);">
                 <p><strong style="color: #6200ea;">Maine Sourced Live Resin Disposable (1g)</strong><br>
                 - $35 for one - $60 for two<br>
                 <span style="color: #00e676;">Strains: Lemon Berry, GMO</span></p>
                 <img src="Images/gmo-dispo.jpeg" alt="GMO Live Resin Disposable" style="max-width: 100%; max-height: 300px; height: auto; width: auto; display: block; margin: 10px auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.2);">
+                <img src="Images/lemonberry-dispo.jpeg" alt="LB Live Resin Disposable" style="max-width: 100%; max-height: 300px; height: auto; width: auto; display: block; margin: 10px auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.2);">
                 <p><strong style="color: #6200ea;">Seréno Hash Rosin Disposable (0.5g)</strong><br>
                 - $30 for one - $55 for two<br>
                 <span style="color: #00e676;">Strains: SOLD OUT </span></p>
@@ -30,10 +32,12 @@ menuButton.addEventListener("click", () => {
                 - King Tree (1g king size joint): $15 for one, $20 for two<br>
                 - Mini Trees (2 - 0.5g joints): $15 for one, $25 for two<br>
                 <span style="color: #00e676;">Strain: Candy Chrome - Infused and coated with Premium Distillate and Kief</span></p>
+                <img src="Images/preroll.jpeg" alt="preroll" style="max-width: 100%; max-height: 300px; height: auto; width: auto; display: block; margin: 10px auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.2);">
                 <p><strong style="color: #6200ea;">High Tides Hash Rosin</strong><br>
                 - $90 2gram bucket SOLD OUT</p>
                 <p><strong style="color: #6200ea;">Maine Sourced Hash Rosin</strong><br>
                 - $40 per gram Animal Tsunami, Cliff House Cannabis $80 2g Bucket Grapes N' Cream</p> 
+            <img src="Images/grapesn.jpeg" alt="Grapes N Cream" style="max-width: 100%; max-height: 300px; height: auto; width: auto; display: block; margin: 10px auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.2);">
             </div>
         `,
         confirmButtonText: "Close",
